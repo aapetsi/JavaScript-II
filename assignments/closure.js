0a75931ae762ec704b8ef6272cf9ca95dbae45ad
Apetsi Ampiah <apetsiampiah@gmail.com>
2019-08-20 11:55:54 +0000

    completed stretch challenge for closure

--- assignments/closure.js
+++ assignments/closure.js
@@ -5,11 +5,11 @@
 // The outer scope can be a parent function, or the top level of the script.
 
 let addBy = 5
-function addBy5(a,b) {
+function addBy5(a, b) {
   return a + b + addBy
 }
 
-console.log(addBy5(5,4))
+console.log(addBy5(5, 4))
 
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
@@ -19,14 +19,21 @@ console.log(addBy5(5,4))
 const counterMaker = () => {
   // IMPLEMENTATION OF counterMaker:
   // 1- Declare a `count` variable with a value of 0. We will be mutating it, so declare it using `let`!
+  let count = 0;
   // 2- Declare a function `counter`. It should increment and return `count`.
+  return function counter() {
+    return count++;
+  }
+
   //      NOTE: This `counter` function, being nested inside `counterMaker`,
   //      "closes over" the `count` variable. It can "see" it in the parent scope!
   // 3- Return the `counter` function.
 };
-// Example usage: const myCounter = counterMaker();
-// myCounter(); // 1
-// myCounter(); // 2
+// Example usage: 
+const myCounter = counterMaker();
+console.log(myCounter())
+console.log(myCounter()); // 1
+console.log(myCounter()); // 2
 
 // ==== Challenge 3: Make `counterMaker` more sophisticated ====
 // It should have a `limit` parameter. Any counters we make with `counterMaker`
@@ -34,7 +41,23 @@ const counterMaker = () => {
 
 // ==== Challenge 4: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
+  let counter = 0
+  return {
+    increment: function () {
+      return counter++
+    },
+    decrement: function () {
+      return counter--
+    }
+  }
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
 };
+
+const myCounterFactory = counterFactory()
+console.log(myCounterFactory)
+console.log(myCounterFactory.increment())
+console.log(myCounterFactory.increment())
+console.log(myCounterFactory.decrement())
+console.log(myCounterFactory.increment())
